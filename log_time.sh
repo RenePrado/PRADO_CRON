cf15f6caf5e79cef8dfffd1ceb621ff655308d60
Rene Angelo Prado <152822689+RenePrado@users.noreply.github.com>
2025-04-09 20:03:02 +0800

    Create log_time.sh

--- log_time.sh
+++ log_time.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-# Append timestamp and username to log.txt
-echo "Current Date and Time: $(date) | User: $GITHUB_ACTOR" >> log.txt
-
-# Count total entries and update log_count.txt
-count=$(wc -l < log.txt)
-echo "Total log entries: $count" > log_count.txt
+echo "Current Date and Time: $(date)" >> log.txt
